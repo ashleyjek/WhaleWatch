20e061928f789a4dac6da429ca1c68607eb51b71
ashley <ashleykimca@gmail.com>
2023-06-29 10:32:46 -0400

    setup

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var e;e=class{constructor(e){this.ele=e,this.ele.innerHTML="<h1>It's ALIVE!!!</h1>",this.handleClick=this.handleClick.bind(this),this.ele.addEventListener("click",this.handleClick)}handleClick(){this.ele.children[0].innerText="Ouch!"}},document.addEventListener("DOMContentLoaded",(()=>{const t=document.getElementById("main");new e(t)}))}();
+//# sourceMappingURL=main.js.map
